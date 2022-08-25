d6fb6ea6619d6c005e889f996c00c71e7fcb301e
carlhwan <111335477+carlhwan@users.noreply.github.com>
2022-08-26 00:58:14 +0900

    all cors allow

--- src/main/java/com/axon/filter/CorsFilter.java
+++ src/main/java/com/axon/filter/CorsFilter.java
@@ -25,8 +25,8 @@ public class CorsFilter implements Filter {
 
         //여기에 내가 허용하고자 하는 클라이언트의 url을 입력해 줍니다.
         //주의사항 "https://myurl.com/" 처럼 마지막에 '/'를 붙이면 CORS에러가 그대로 발생하게 됩니다.
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        //response.setHeader("Access-Control-Allow-Origin", "*");  //이렇게 해서 모든 요청에 대해서 허용할 수도 있습니다.
+        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*");  //이렇게 해서 모든 요청에 대해서 허용할 수도 있습니다.
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
